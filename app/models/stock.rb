cbb3e0ea022b38f6d62b871951b23951bfbecde6
DataNick <DataNick@users.noreply.github.com>
2015-10-09 11:45:58 -0400

    Added class method to Stock to search stock price from Yahoo api

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,14 @@
 class Stock < ActiveRecord::Base
+
+  def self.update_current_price(tickers)
+    yahoo_tickers = tickers.map {|x| "'" + x + "'"}.join(', ')
+    if yahoo_tickers.count > 0
+      url = 'https://query.yahooapis.com/v1/public/yql?q='
+      url += URI.encode("SELECT * FROM yahoo.finance.quotes WHERE symbol IN (#{yahoo_ticker})")
+      url += '&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
+      data = open(url, {:read_timeout=>3}).read_timeout
+      parse_today_data(data)
+    end
+  end
+
 end
