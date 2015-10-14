8d35b592f691c8c72fcfe63676ed50d0b08c3bec
DataNick <DataNick@users.noreply.github.com>
2015-10-14 15:27:59 -0400

    Ran test on stock_quote_fetcher

--- spec/lib/stock_quote_fetcher_spec.rb
+++ spec/lib/stock_quote_fetcher_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe StockQuoteFetcher, type: :model do
     it { should be a new(StockQuoteFetcher) }
   end
 
+  describe "stock_quote_fetcher single argument" do
+    subject { StockQuoteFetcher.new(Stock.new(ticker: "GOOG")).stock_fetch}
+    it {should be new(Hash("GOOG" =>{name: String.new, price: bigdecimal.new, date: Time.new} ))}
+  end
+
 end
 
 
