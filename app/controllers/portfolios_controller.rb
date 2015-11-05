f1dbc205cd44da1020cecf1a073e48af0ea47be4
DataNick <DataNick@users.noreply.github.com>
2015-11-05 00:05:56 -0500

    Created create, destroy, set_portfolio and portfolio_params methods

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,14 +1,40 @@
 class PortfoliosController < ApplicationController
-	def show
+	before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+
+  def show
 	end
 
 	def new
     @portfolio = Portfolio.new
+    @portfolio.portfolio_stock.new
 	end
 
 	def update
 	end
 
+  def edit
+  end
+
+  def destroy
+  end
+
 	def create
+    @portfolio = current_user.portfolios.build(portfolio_params)
+    @portfolio.user_id = current_user.id
+    if portfolio.save
+      respond_with(@portfolio)
+    else
+      render :new
+    end
 	end
+
+  private
+
+  def set_portfolio
+    @portfolio = Portfolio.find(params[:id])
+  end
+
+  def portfolio_params
+    params.require(:portfolio).permit(:name, :cash, :user_id)
+  end
 end
